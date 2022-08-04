c818ea5dd74c2041e98b32dcaef652eab14d50b9
kakaprodo <promskakaprodo45@gmail.com>
2022-08-04 13:13:20 +0200

    Support laravel functionalities

---
+++ src/CustomDataServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kakaprodo\CustomData;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomDataServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
